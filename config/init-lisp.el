375a273bf40c8b370650142c6fc0ce90659b2f21
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2014-11-29 22:01:26 +0100

    Setup init things.

---
+++ config/init-lisp.el
@@ -0,0 +1,9 @@
+;; init-lisp.el -- Provide various lisps configurations (racket, emacs-lisp, clojure)
+
+(require-package 'cider)
+(require-package 'geiser)
+(require-package 'racket-mode)
+(require-package 'slime)
+
+(provide 'init-lisp)
+
