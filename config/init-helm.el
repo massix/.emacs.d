4d1baf7402a81034febdf16c443844e8a7b1dd58
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2014-11-29 22:24:54 +0100

    Configure helm properly.

--- config/init-helm.el
+++ config/init-helm.el
@@ -8,11 +8,17 @@
 (setq helm-bookmark-show-location t)
 (setq helm-buffers-fuzzy-matching t)
 
-
 (after "projectile-autoloads"
   (require-package 'helm-projectile))
 
 (after "company-autoloads"
   (require-package 'helm-company))
+
 (require 'helm-grep)
+(require 'helm-config)
+
+(global-set-key (kbd "C-c h O") 'helm-occur)
+(global-set-key (kbd "C-c h o") 'helm-swoop)
+(global-set-key (kbd "C-c h g") 'helm-do-grep)
+
 (provide 'init-helm)
