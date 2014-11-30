a5142198efb71de351b5a33b32529c0eb0f45790
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2014-11-30 18:45:33 +0100

    Better defaults for Jabber.

--- config/init-jabber.el
+++ config/init-jabber.el
@@ -10,18 +10,20 @@
   "Default password to use on jabber"
   :group 'my/dotemacs)
 
-(setq jabber-avatar-cache-directory (concat my/dotemacs-cache-directory "jabber/"))
+(setq jabber-avatar-cache-directory
+      (concat my/dotemacs-cache-directory "jabber/"))
 
 (setq jabber-account-list
-      '((my/jabber-user-name
-         (:password . my/jabber-password)
+      `((,my/jabber-user-name
+         (:password . ,my/jabber-password)
+         (:network-server . "talk.google.com")
          (:port . 443)
          (:connection-type . ssl))))
 
-(add-hook 'jabber-post-connect-hooks 'jabber-mode-line)
+(add-hook 'jabber-post-connect-hooks
+          (lambda () (jabber-mode-line-mode 1)))
 (add-hook 'jabber-post-disconnect-hook
-          (lambda () (jabber-mode-line -1)))
-
+          (lambda () (jabber-mode-line-mode -1)))
 
 (provide 'init-jabber)
 
