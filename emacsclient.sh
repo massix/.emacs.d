a9db902a338eb83abb2ab5d5d9e50681dd1d7270
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2014-12-01 13:42:00 +0100

    Added a wrapper script for emacs

---
+++ emacsclient.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Depending on the link used, open either the gui or the terminal
+if [[ $(basename $0) = "e" ]]; then
+    arguments="-nw"
+elif [[ $(basename $0) = "eframe" ]]; then
+    arguments="-c --no-wait"
+elif [[ $(basename $0) = "eeval" ]]; then
+    arguments="--eval"
+elif [[ $(basename $0) = "ecd" || $(basename $0) = "efile" ]]; then
+    arguments=""
+else
+    arguments="--no-wait"
+fi
+
+if [[ $(basename $0) = "ecd" ]]; then
+    dir=$(${HOME}/bin/eeval '(file-name-directory (buffer-file-name (window-buffer)))' | tr -d \")
+    echo $dir
+fi
+
+if [[ $(basename $0) = "efile" ]]; then
+    file=$(${HOME}/bin/eeval '(buffer-file-name (window-buffer))' | tr -d \")
+    echo $file
+fi
+
+# Avoid running the emacsclient for ecd and efile `functions'
+[[ ! -z ${arguments} ]] && /usr/local/bin/emacsclient -a \"\" ${arguments} "$@"
